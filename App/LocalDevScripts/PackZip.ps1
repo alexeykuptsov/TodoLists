8edad24ad97720afe1f7cee83bd7063236a59f09
Alexey Kuptsov <alxkuptsov@gmail.com>
2023-01-13 18:21:08 +0300

    + WpfApp

---
+++ App/LocalDevScripts/PackZip.ps1
@@ -0,0 +1,21 @@
+﻿Push-Location (Join-Path (Split-Path $MyInvocation.MyCommand.Path) ".." -Resolve)
+try
+{
+    . LocalDevScripts/Common.ps1
+
+    $PsqlFolderName = "bin\Debug\net7.0\pgsql"
+    if (Test-Path $PsqlFolderName)
+    {
+        Remove-Item -Recurse -Force $PsqlFolderName
+    }
+    New-Item -ItemType Directory -Force -Path $PsqlFolderName
+
+    $excludes = "pgAdmin 4"
+    Get-ChildItem "C:\Tools\postgresql-15.1-1-windows-x64-binaries\pgsql" -Directory |
+            Where-Object{$_.Name -notin $excludes} |
+            Copy-Item -Destination $PsqlFolderName -Recurse -Force
+}
+finally
+{
+    Pop-Location
+}
---
+++ WpfApp/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private Process? myAppProcess;
+        
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            myAppProcess = Process.Start(new ProcessStartInfo
+            {
+                FileName = @"C:\Code\ak\github\TodoLists\App\bin\Debug\net7.0\TodoLists.App.exe",
+                Arguments = "",
+                WorkingDirectory = @"C:\Code\ak\github\TodoLists\App\bin\Debug\net7.0\",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                RedirectStandardInput = true,
+                CreateNoWindow = true,
+            });
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            if (myAppProcess != null)
+            {
+                myAppProcess.Kill();
+            }
+            
+            base.OnExit(e);
+        }
+    }
+}
